d6913a593636f37fc957f7c4daefe41dc89bfaf5
Zhao Rui <e1101901@nus.edu.sg>
2023-03-05 14:11:20 +0800

    debug for update github profile

--- github_myprofile_updater/update.py
+++ github_myprofile_updater/update.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     _intro = open(f'{base_dir}/intro.md').read().strip()
     _homepage = open(f'{base_dir}/homepage.md').read().strip()
     _news = open(f'{base_dir}/news.md').read().strip()
-    with open('_README.md', 'w') as f:
+    with open('README.md', 'w') as f:
         f.write(_intro)
         f.write('\n\n##')
         f.write(_homepage)
