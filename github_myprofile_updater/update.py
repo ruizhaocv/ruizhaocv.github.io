15086d78c744258df62b359076cb9b33a1747b1f
Zhao Rui <e1101901@nus.edu.sg>
2023-03-05 14:41:09 +0800

    update icon

--- github_myprofile_updater/update.py
+++ github_myprofile_updater/update.py
@@ -1,9 +1,12 @@
 if __name__ == '__main__':
+    _header = '## Hi there 👋'
     base_dir = '../_pages/includes/'
     _intro = open(f'{base_dir}/intro.md').read().strip()
     _homepage = open(f'{base_dir}/homepage.md').read().strip()
     _news = open(f'{base_dir}/news.md').read().strip()
     with open('README.md', 'w') as f:
+        f.write(_header)
+        f.write('\n\n')
         f.write(_intro)
         f.write('\n\n##')
         f.write(_homepage)
