097f3273f306f6ca6a891cc1d221fdf0a0b182fb
Rui Zhao <ruizhaoipc@163.com>
2025-03-06 12:35:25 +0800

    Update update.py

--- github_myprofile_updater/update.py
+++ github_myprofile_updater/update.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
         f.write(_intro)
         f.write('\n\n##')
         f.write(_homepage)
-        f.write('\n\n##')
+        f.write('\n\n')
         # f.write(_news)
         f.write('<img align="left" src="https://github-readme-stats-one-bice.vercel.app/api?username=ruizhaocv&show_icons=true&include_all_commits=true&count_private=true&role=OWNER,ORGANIZATION_MEMBER,COLLABORATOR&theme=tokyonight">')
